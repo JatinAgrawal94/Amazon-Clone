dfc6d91d3d4b8db4f23ad529e28cfe6445ed36cc
Jatin Agrawal <jatinagrawal0801@gmail.com>
2021-01-16 13:45:12 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+console.log(process.env.MONGODB_URL);
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
